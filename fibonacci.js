fb004e7ab025e3536ba47f13a53a40d9f2ae9d85
Carlos Andrés <ca.castaneda@outlook.com>
2023-03-25 23:12:08 -0500

    Add fibonacci array recursive function

--- fibonacci.js
+++ fibonacci.js
@@ -2,7 +2,7 @@
 
 function fibs(number) {
     let numbers = [];
-    for (let i = 0; i < number; i++) {
+    for (let i = 0; i <= number; i++) {
         if (i === 0) {
             numbers.push(0);
         } else if (i === 1) {
@@ -17,3 +17,16 @@ function fibs(number) {
 console.log(fibs(8)); //[0, 1, 1, 2, 3, 5, 8, 13];
 
 // Recursion
+function fibsRec(n) {
+    return n === 0
+        ? [].concat(0)
+        : n === 1
+        ? [].concat(fibsRec(n - 1), [1])
+        : [].concat(
+              fibsRec(n - 1),
+              fibsRec(n - 1)[fibsRec(n - 1).length - 1] +
+                  fibsRec(n - 1)[fibsRec(n - 1).length - 2]
+          );
+}
+
+console.log(fibsRec(8));
